bbc6f3a8abac31217e4203342b13fdbcc4e16ddb
Jack Knowles <jackmhknowles@gmail.com>
2022-07-27 16:44:03 +0100

    all passing but still want to get a variation of the last extension

--- models/traveller.js
+++ models/traveller.js
@@ -3,31 +3,48 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-  return this.journeys.map(journey => journey.startLocation)
+  return this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  return this.journeys.map(journey => journey.endLocation)
+  return this.journeys.map(journey => journey.endLocation);
 
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-  return this.journeys.filter(journey => journey.transport === transport)
+  return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-  return this.journeys.filter(journey => journey.distance >= minDistance)
+  return this.journeys.filter(journey => journey.distance >= minDistance);
 
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-  return this.journeys.reduce((total_distance, journey) => total_distance + journey.distance, 0)
+  return this.journeys.reduce((total_distance, journey) => total_distance + journey.distance, 0);
 
 };
+
+// Extension Version 1
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  return this.journeys.map(journey => journey.transport)
+  list = this.journeys.map(journey => journey.transport);
+  return [...new Set(list)];
+
 };
+
+// Extension Version 2 NOT WORKING ????
+
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+
+//    unique_list = this.journeys.filter((journey, index, array) => {
+//      array.indexOf(journey) == index;
+//   });
+
+//   return unique_list
+// };
+
+
 
 
 module.exports = Traveller;
